d0d0b29ed691c9f0b64e438b2a5216da54e91085
thanhtrung2000 <trungphampttt@gmail.com>
2022-07-06 21:49:05 +0700

    api

---
+++ lib/services/promotion_services.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:loyalty_platform_mobile_flutter/object/promotion.dart';
+
+class PromotionService {
+  getPromotion() async {
+    var response = await http.get(Uri.parse(
+        'https://api-promotion-engine.reso.vn/api/promotions?PageSize=10&PageIndex=1&BrandId=dbe5e8e0-363b-4119-a145-6c7775abf4d3&status=0'));
+    var jsonData = jsonDecode(response.body);
+    List<Promotion> promotions = [];
+
+    for (var u in jsonData) {
+      Promotion promotion =
+          Promotion(u["imgUrl"], u["description"], u["promotionName"]);
+      promotions.add(promotion);
+    }
+    print(promotions.length);
+    return promotions;
+  }
+}
